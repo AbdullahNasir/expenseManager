140e8f9a10d9043a614f0c14a0cc273278b8c5b0
Abdullah Nasir <abdullah555@yahoo.com>
2014-08-23 12:17:09 +0500

    added views for expense & income and modified routes.js

--- packages/transactions/public/routes/transactions.js
+++ packages/transactions/public/routes/transactions.js
@@ -2,9 +2,17 @@
 
 angular.module('mean.transactions').config(['$stateProvider',
     function($stateProvider) {
-        $stateProvider.state('transactions example page', {
-            url: '/transactions/example',
+        $stateProvider.state('transactions', {
+            url: '/transactions',
             templateUrl: 'transactions/views/index.html'
+        });
+        $stateProvider.state('transactions.expense',{
+            url:'/transactions/expense',
+            templateUrl:'transactions/views/expense.html'
+        });
+        $stateProvider.state('transactions.income',{
+            url:'/transactions/income',
+            templateUrl:'transactions/views/income.html'
         });
     }
 ]);
